44cea07cfa2a8c4b92917cee382537428dbf33cb
Hawk Weisman <hi@hawkweisman.me>
2015-11-30 14:54:57 -0500

    It moves!

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use glium::Surface;
 use glium::index;
 
 #[derive(Copy, Clone)]
-struct Vertex {
-    position: [f32; 2],
-}
+struct Vertex{ position: [f32; 2] }
+
+const TRI_SPIN_SPEED: f32 = 0.001;
 
 implement_vertex!(Vertex, position);
 
@@ -24,9 +24,10 @@ fn main() {
         #version 140
 
         in vec2 position;
+        uniform mat4 matrix;
 
         void main() {
-            gl_Position = vec4(position, 0.0, 1.0);
+            gl_Position = matrix * vec4(position, 0.0, 0.1);
         }
     "#;
 
@@ -37,11 +38,59 @@ fn main() {
         out vec4 color;
 
         void main() {
-            color = vec4(1.0, 1.0, 1.0, 1.0);
+            color = vec4(1.0, 1.0, 0.0, 1.0);
         }
     "#;
 
+    // Our shape. It's just a big triangle
+    let shape = vec![ Vertex { position: [-0.5,  0.5 ]  }
+                    , Vertex { position: [ 0.0,  0.5 ] }
+                    , Vertex { position: [ 0.5,  0.25] }
+                    // , Vertex { position: (0.0, 0.0) }
+                    ];
+
+    let program
+        = glium::Program::from_source( &display
+                                     , vertex_src
+                                     , frag_src
+                                     , None)
+            .expect("Could not compile shader program!");
+
+    let mut theta: f32 = 0.0;
+
     loop {
+
+        let mut frame = display.draw();
+
+        // This is a rotation matrix for making the square spin.
+        let uniforms = uniform! {
+            matrix: [ [ theta.cos(), theta.sin(), 0.0, 0.0]
+                    , [-theta.sin(), theta.cos(), 0.0, 0.0]
+                    , [0.0         , 0.0        , 1.0, 0.0]
+                    , [0.0         , 0.0        , 0.0, 1.0f32],
+                    ]
+                };
+
+        let vertex_buffer
+            = glium::VertexBuffer::new(&display, &shape)
+                .expect("Could not create vertex buffer!");
+
+        let indices
+            = index::NoIndices(index::PrimitiveType::TrianglesList);
+
+        frame.clear_color(0.0, 0.0, 0.0, 1.0); // clear the display
+        frame.draw( &vertex_buffer
+                  , &indices
+                  , &program
+                  , &uniforms
+                  , &Default::default()
+                  ).expect("Could not draw shaders!");
+        frame.finish() // draw this frame, panic if the frame was not drawn
+             .expect("Could not finish frame!");
+
+        theta = if theta + TRI_SPIN_SPEED < 360.0 { theta + TRI_SPIN_SPEED }
+                else { 0.0 };
+
         // Iterate through the events that the window recieved this frame
         for event in display.poll_events() {
             match event {
@@ -51,36 +100,5 @@ fn main() {
               , _ => {}
             }
         };
-
-        let mut frame = display.draw();
-
-        let shape = vec![ Vertex { position: [-0.5, -0.5] }
-                        , Vertex { position: [-0.5,  0.5] }
-                        , Vertex { position: [0.5,  -0.5]}
-                        ];
-
-        let vertex_buffer
-            = glium::VertexBuffer::new(&display, &shape)
-                .expect("Could not create vertex buffer!");
-
-        let indices
-            = index::NoIndices(index::PrimitiveType::TrianglesList);
-
-        let program
-            = glium::Program::from_source( &display
-                                         , vertex_src
-                                         , frag_src
-                                         , None)
-                .expect("Could not compile shader program!");
-
-        frame.clear_color(0.0, 0.0, 0.0, 1.0); // clear the display
-        frame.draw( &vertex_buffer
-                  , &indices
-                  , &program
-                  , &glium::uniforms::EmptyUniforms
-                  , &Default::default()
-                  ).expect("Could not draw shaders!");
-        frame.finish() // draw this frame, panic if the frame was not drawn
-             .expect("Could not finish frame!");
     }
 }
